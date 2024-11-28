a17f64528d710f37913a16b80e1ec630972a1503
Ali Khorsandfard <secure73@gmail.com>
2024-11-28 11:45:21 +0100

    layout finish

--- about.php
+++ about.php
@@ -1,8 +1,15 @@
 <?php require_once('./assets/layout/header.php'); ?> 
 <main class="main">
 
+    <!-- Contact Section -->
+    <section id="contact" class="contact section">
 
-Mein CV
+      <!-- Section Title -->
+      <div class="container section-title" data-aos="fade-up">
+          Mein CV
+
+      </div>
+      </section>
 
 
 
